7586c07ec2e6265b99caaef0cf197f2cc8e291a3
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-09-24 23:21:56 +0530

    Time: 99 ms (79.81%) | Memory: 83.4 MB (96.24%) - LeetSync

--- 2504-find-all-good-indices/find-all-good-indices.cpp
+++ 2504-find-all-good-indices/find-all-good-indices.cpp
@@ -1,30 +1,60 @@
 class Solution {
 public:
     vector<int> goodIndices(vector<int>& nums, int k) {
-        int n = nums.size();
+        // int n = nums.size();
 
-        vector<int>left(n,0);
-        vector<int>right(n,0);
-        vector<int>ans;
+    // O(1) solution
 
-        for (int i=1; i<n; i++) {
-            if (nums[i] <= nums[i-1]) {
-                left[i] = left[i-1] + 1;
-            }
+     int n = nums.size();
+        vector<int> res;
+        int c1 = 1, c2 = 1;
+        for (int j=2; j<k; j++)
+        {
+            if (nums[j] >= nums[j-1])
+                c2++;
+            else
+                c2 = 1;
         }
-
-        for (int i=n-2; i>=0; i--) {
-            if (nums[i] <= nums[i+1]) {
-                right[i] = right[i+1] + 1;
-            }
+        int i = 0;
+        while (i+k<n) 
+        {
+            if (nums[i+k] >= nums[i+k-1])
+                c2++;
+            else
+                c2 = 1;
+            if (i>=2 && (nums[i-1] <= nums[i-2]))
+                c1++;
+            else
+                c1 = 1;
+            if (c1 >= k && c2 >=k && i >=k)
+                res.push_back(i);
+            i++;
         }
+        return res;
 
-        for (int i=k; i<n-k; i++) {
-            if (min(left[i-1], right[i+1]) >= k-1) {
-                ans.push_back(i);
-            }
-        }
+    // O(n) Solution
+        // vector<int>left(n,0);
+        // vector<int>right(n,0);
+        // vector<int>ans;
 
-        return ans;
+        // for (int i=1; i<n; i++) {
+        //     if (nums[i] <= nums[i-1]) {
+        //         left[i] = left[i-1] + 1;
+        //     }
+        // }
+
+        // for (int i=n-2; i>=0; i--) {
+        //     if (nums[i] <= nums[i+1]) {
+        //         right[i] = right[i+1] + 1;
+        //     }
+        // }
+
+        // for (int i=k; i<n-k; i++) {
+        //     if (min(left[i-1], right[i+1]) >= k-1) {
+        //         ans.push_back(i);
+        //     }
+        // }
+
+        // return ans;
     }
 };
