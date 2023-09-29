0451d60574ed0cd8eb2e95b516500bd9d08c3043
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-09-29 11:11:12 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (35.42%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.cpp
@@ -11,34 +11,48 @@
  */
 class Solution {
 public:
+
+    void recursion (TreeNode *root, int level, vector<int> &ans) {
+        if (root == NULL) {
+            return;
+        }
+        if (ans.size() < level) {
+            ans.push_back(root->val);
+        }
+        recursion(root->right, level+1, ans);
+        recursion(root->left, level+1, ans);
+    }
     vector<int> rightSideView(TreeNode* root) {
         vector<int>ans;
-
-        if (root == NULL) {
-            return ans;
-        }
-
-        queue<TreeNode*>q;
-        q.push(root);
-
-        while(!q.empty()) {
-            int size = q.size();
-
-            for (int i=0; i<size; i++) {
-                TreeNode* node = q.front();
-                if (i== size-1) {
-                    ans.push_back(node->val);
-                }
-                if (node->left) {
-                    q.push(node->left);
-                }
-                if (node->right) {
-                    q.push(node->right);
-                }
-                q.pop();
-            }
-        }
-
+        recursion(root, 1, ans);
         return ans;
+        // vector<int>ans;
+
+        // if (root == NULL) {
+        //     return ans;
+        // }
+
+        // queue<TreeNode*>q;
+        // q.push(root);
+
+        // while(!q.empty()) {
+        //     int size = q.size();
+
+        //     for (int i=0; i<size; i++) {
+        //         TreeNode* node = q.front();
+        //         if (i== size-1) {
+        //             ans.push_back(node->val);
+        //         }
+        //         if (node->left) {
+        //             q.push(node->left);
+        //         }
+        //         if (node->right) {
+        //             q.push(node->right);
+        //         }
+        //         q.pop();
+        //     }
+        // }
+
+        // return ans;
     }
 };
