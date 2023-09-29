5538f7746b6e1a0cc3820baab37ec0f53f11146b
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-09-29 11:18:59 +0530

    Time: 13 ms (18.69%) | Memory: 21.9 MB (33.52%) - LeetSync

--- 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
+++ 513-find-bottom-left-tree-value/find-bottom-left-tree-value.cpp
@@ -11,33 +11,49 @@
  */
 class Solution {
 public:
-    int findBottomLeftValue(TreeNode* root) {
-        int ans;
+    void solve(TreeNode* root, int &ans, int &m, int h) {
         if (root == NULL) {
-            return ans;
+            return;
         }
-
-        queue<TreeNode*>q;
-        q.push(root);
-
-        while(!q.empty()) {
-            int size = q.size();
-
-            for (int i=0; i<size; i++) {
-                TreeNode* node = q.front();
-                if (i== 0) {
-                    ans = node->val;
-                }
-                if (node->left) {
-                    q.push(node->left);
-                }
-                if (node->right) {
-                    q.push(node->right);
-                }
-                q.pop();
-            }
+        if (m<h) {
+            ans = root->val;
+            m=h;
         }
-
+        solve(root->left, ans, m, h+1);
+        solve(root->right, ans, m, h+1);
+    }
+    int findBottomLeftValue(TreeNode* root) {
+        int m = -1;
+        int h=0;
+        int ans;
+        solve(root, ans, m, h);
         return ans;
+        // int ans;
+        // if (root == NULL) {
+        //     return ans;
+        // }
+
+        // queue<TreeNode*>q;
+        // q.push(root);
+
+        // while(!q.empty()) {
+        //     int size = q.size();
+
+        //     for (int i=0; i<size; i++) {
+        //         TreeNode* node = q.front();
+        //         if (i== 0) {
+        //             ans = node->val;
+        //         }
+        //         if (node->left) {
+        //             q.push(node->left);
+        //         }
+        //         if (node->right) {
+        //             q.push(node->right);
+        //         }
+        //         q.pop();
+        //     }
+        // }
+
+        // return ans;
     }
 };
