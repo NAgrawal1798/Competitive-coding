246429c3f589ce1d36e0ee69a84195fae0b0cd48
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-10-02 19:47:14 +0530

    Time: 2 ms (61.83%) | Memory: 6.3 MB (15.98%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
+int dp[20]{};
     int numTrees(int n) {
-        vector<int> dp(n+1);
-        dp[0]=1;
-        for(int i=1;i<=n;i++)
-        {
-            int sum=0;
-            for(int j=0;j<i;j++)
-            {
-                sum+=dp[j]*dp[i-j-1];
-            }
-            dp[i]=sum;
+        if (n<=1) {
+            return 1;
+        }
+        if (dp[n]) {
+            return dp[n];
+        }
+        for (int i=1; i<=n; i++) {
+            dp[n] += numTrees(i-1) * numTrees(n-i);
         }
         return dp[n];
     }
