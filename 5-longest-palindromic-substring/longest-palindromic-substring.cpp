70dff71025ebf0404303a6a35a7bf8871190b9b7
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-10-04 21:41:40 +0530

    Time: 543 ms (20.59%) | Memory: 23.3 MB (32.66%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        int n = s.size();
-        vector<vector<bool>> dp(s.size(), vector<bool>(n, false));
-        int ans=0, start;
-        for(int i=0; i<n; ++i)
-            for(int j=0; j+i<n; ++j){
-                if(i==0)    dp[j][j+i] = true;
-                else if(i==1 and s[j]==s[j+i]) dp[j][j+i] = true;
-                else if(i>1 and s[j]==s[j+i] and dp[j+1][j+i-1]) dp[j][j+i] = true;
-                if(dp[j][j+i])  ans = i, start=j;
-            }
-        return s.substr(start,ans+1);
+       int n = s.size();
+
+       vector<vector<bool>>dp(n, vector<bool>(n, false));
+
+       int ans = 0;
+       int start;
+
+       for (int iteration=0; iteration<n; iteration++) {
+           for (int j=0; j+iteration<n; j++) {
+               if (iteration==0) {
+                   dp[j][j] = true;
+               } else if (iteration==1 && s[j] == s[j+1]) {
+                   dp[j][j + 1] = true;
+               } else if (iteration > 1 && dp[j+1][j+iteration -1] && s[j] == s[j + iteration]) {
+                   dp[j][j + iteration] = true;
+               }
+
+               if (dp[j][j+iteration]) {
+                   ans = iteration;
+                   start = j;
+               }
+           }
+       }
+
+       return s.substr(start, ans+1);
     }
 };
