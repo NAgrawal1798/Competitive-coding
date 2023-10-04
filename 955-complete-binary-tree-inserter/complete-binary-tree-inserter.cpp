5ba6952aeffd0800b53eb1ecdd3f37e508bd8524
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-10-05 00:32:31 +0530

    Time: 15 ms (75.18%) | Memory: 22.8 MB (38.07%) - LeetSync

--- 955-complete-binary-tree-inserter/complete-binary-tree-inserter.cpp
+++ 955-complete-binary-tree-inserter/complete-binary-tree-inserter.cpp
@@ -1,13 +1,3 @@
-/*
-    https://leetcode.com/problems/complete-binary-tree-inserter/submissions/
-    
-    Insert: TC: O(N), SC: O(1)
-    get: TC: O(1)
-    
-    Idea is to to BFS and look for the first node that has 0/1 child, and 
-    make the input value as a new node for that parent.
-*/
-
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -20,52 +10,49 @@
  * };
  */
 class CBTInserter {
-private:
-    TreeNode* root_ = nullptr;
 public:
-    CBTInserter(TreeNode* root) {
-        root_ = root;
-    }
-    
-    int insert(int v) {
-        // Use bfs for insertion
-        queue<TreeNode*> q;
-        q.emplace(root_);
-        TreeNode* curr = nullptr;
-        
-        while(!q.empty()) {
-            curr = q.front();
-            q.pop();
-            
-            // check if any of its child is a nullptr
-            // if a node is found to be not having a child, then
-            // add the current node as its child
-            if(!curr->left) {
-                curr->left = new TreeNode(v);
-                break;
-            }
-            if(!curr->right) {
-                curr->right = new TreeNode(v);
-                break;
-            }
-            
-            if(curr->left)
-                q.emplace(curr->left);
-            if(curr->right)
-                q.emplace(curr->right);
+    queue<TreeNode*>q;
+    TreeNode* root1 = nullptr;
+
+    CBTInserter(TreeNode* root)
+    {
+        root1 = root;
+        q.push(root);
+        while(!q.empty())
+        {
+            TreeNode* f = q.front();
+            if(f->left)q.push(f->left);
+            if(f->right)q.push(f->right);
+            if(f->left && f->right)q.pop();
+            else
+            break;
         }
-        
-        return curr->val;
     }
     
-    TreeNode* get_root() {
-        return root_;
+    int insert(int val) 
+    {
+        TreeNode* curr = new TreeNode(val);
+        TreeNode* f = q.front();
+        if(f->left == nullptr)f->left = curr;
+        else
+        {
+            f->right = curr;
+            q.pop();
+        }
+        q.push(curr);
+        return f->val;
+        
+    }
+    
+    TreeNode* get_root() 
+    {
+        return root1;   
     }
 };
 
 /**
  * Your CBTInserter object will be instantiated and called as such:
  * CBTInserter* obj = new CBTInserter(root);
- * int param_1 = obj->insert(v);
+ * int param_1 = obj->insert(val);
  * TreeNode* param_2 = obj->get_root();
  */
