f681a60c73a934db943ad7ad28bcc9a13de56c1b
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-11-01 17:47:57 +0530

    Time: 406 ms (5.05%) | Memory: 106 MB (5.23%) - LeetSync

--- 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.cpp
+++ 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.cpp
@@ -14,9 +14,9 @@ public:
         for (int i=1; i<=n; i++) {
             int willBreak = f(i-1, e-1, dp);
             int notBreak = f(n-i, e, dp);
-            ans = min(ans, max(willBreak, notBreak));
+            ans = min(ans, max(willBreak, notBreak) + 1);
         }
-        return dp[n][e] = ans+1;
+        return dp[n][e] = ans;
 
     }
     int twoEggDrop(int n) {
