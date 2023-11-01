09b2b535225f5359c6119e448fb38baac1d1278e
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-11-01 17:30:54 +0530

    Time: 417 ms (5.05%) | Memory: 105.9 MB (5.23%) - LeetSync

--- 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.cpp
+++ 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
-    int twoEggDrop(int n) {
-        int a=0,sum=0;
-        for(int i=0;i<=n;i++){
-            sum+=i;
-            if(sum>=n){
-                a=i;
-                break;
-            }
+    int f(int n ,int e , vector<vector<int>>&dp) {
+        if (n==0 || n==1) {
+            return n;
         }
-        return a;
+        if (e==1) {
+            return n;
+        }
+        if (dp[n][e] != -1) {
+            return dp[n][e];
+        }
+        int ans = INT_MAX;
+        for (int i=1; i<=n; i++) {
+            int willBreak = f(i-1, e-1, dp);
+            int notBreak = f(n-i, e, dp);
+            ans = min(ans, max(willBreak, notBreak));
+        }
+        return dp[n][e] = ans+1;
+
+    }
+    int twoEggDrop(int n) {
+        vector<vector<int>>dp(10001, vector<int>(3,-1));
+        return f(n,2, dp);
     }
 };
