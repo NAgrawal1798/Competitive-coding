a6e693907c8717cac933c899f5a26bf832f25422
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-11-04 14:17:59 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (74.53%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
+    vector <string> ans;
+    string current;
 
-    // Function to check if a string of parentheses is balanced or not
-    bool isBalanced(string s) {
-        int balance = 0;
-        for (char c : s) {
-            if (c == '(') {
-                balance++;
-            } else if (c == ')') {
-                balance--;
-                if (balance < 0) {
-                    return false;
-                }
-            }
-        }
-
-        return balance == 0;
-    }
-
-    void generateParenthesisHelper(int n, vector<string>& combinations, string current, int open, int close) {
-        if (current.length() == n*2) {
-            if (isBalanced(current)) {
-                combinations.push_back(current);
-            }
+    void solve (int index, int delta, int len)
+    {
+        if (delta < 0)
+        {
             return;
         }
 
-        if (open < n) {
-            generateParenthesisHelper(n, combinations, current + "(", open + 1, close);
+        if (index == len)
+        {
+            if (delta == 0)
+            {
+                ans.push_back (current);
+            }
+
+            return;
         }
-        if (close < open) {
-            generateParenthesisHelper(n, combinations, current + ")", open, close + 1);
-        }
+
+        current.push_back ('(');
+        solve (index + 1, delta + 1, len);
+        current.pop_back();
+
+        current.push_back (')');
+        solve (index + 1, delta - 1, len);
+        current.pop_back();
     }
-    vector<string> generateParenthesis(int n) {
-        // store result
-        vector<string>combinations;
-        generateParenthesisHelper(n, combinations, "", 0, 0);
-        return combinations;
+
+    vector <string> generateParenthesis (int n)
+    {
+        solve (0, 0, n + n);
+
+        return ans;
     }
 };
