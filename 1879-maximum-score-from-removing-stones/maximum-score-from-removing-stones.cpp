4dcb84c7e8a84617eac3f3c83226b6158be4cd9e
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-11-05 12:33:43 +0530

    Time: 82 ms (55.77%) | Memory: 6.6 MB (10.31%) - LeetSync

--- 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.cpp
+++ 1879-maximum-score-from-removing-stones/maximum-score-from-removing-stones.cpp
@@ -1,32 +1,35 @@
 class Solution {
 public:
-    // We can use dp solution
-    // vector<vector<vector<int>>> dp;
-    
-    int helper(int a, int b, int c) {
-        if (a <= 0 && b <= 0) return 0;
-        if (a <= 0 && c <= 0) return 0;
-        if (b <= 0 && c <= 0) return 0;
-        
-        // if (dp[a][b][c] != -1) {
-        //     return dp[a][b][c];
-        // }
-        
-        int score = 0;
-        score = max(score, (a > 0 && b > 0) ? 1 + helper(a - 1, b - 1, c) : 0);
-        score = max(score, (b > 0 && c > 0) ? 1 + helper(a, b - 1, c - 1) : 0);
-        score = max(score, (a > 0 && c > 0) ? 1 + helper(a - 1, b, c - 1) : 0);
-        
-        // dp[a][b][c] = score;
-        
-        return score;
+    int maximumScore(int a, int b, int c) {
+        int ans=0;
+        priority_queue<int> pq;
+        // pushing all three number to track the first two largest score
+        pq.push(a);
+        pq.push(b);
+        pq.push(c);
+        // we will continue to increase the score by one 
+        // until two non empty piles is present
+        while(pq.top()>0){
+            int first;
+            if(pq.top()>0){
+                first = pq.top();
+                pq.pop();
+            }
+            int second;
+            // we are checking if the second pile is non-empty or not
+            if(pq.top()>0){
+                second = pq.top();
+                pq.pop();
+            }
+            // if second pile is empty then we cannot move further
+            // and break the loop
+            else{
+                break;
+            }
+            pq.push(first-1);
+            pq.push(second-1);
+            ans++;
+        }
+        return ans;
     }
-
-   int maximumScore(int a, int b, int c) {
-    if (a < b)
-        return maximumScore(b, a, c);
-    if (b < c)
-        return maximumScore(a, c, b);
-    return b == 0 ? 0 : 1 + maximumScore(a - 1, b - 1, c); 
-}
 };
