d792cf46725d4d36c019f8f16a0872e7888e7836
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-11-05 18:47:23 +0530

    Time: 361 ms (67.57%) | Memory: 144 MB (51.35%) - LeetSync

--- 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.cpp
+++ 1834-minimum-number-of-people-to-teach/minimum-number-of-people-to-teach.cpp
@@ -1,55 +1,51 @@
 class Solution {
 public:
     int minimumTeachings(int n, vector<vector<int>>& languages, vector<vector<int>>& friendships) {
-        int size = friendships.size();
-        vector<pair<int, int>> p;
-        unordered_map<int, int> mp;
+        int m = languages.size();
 
-        // Iterate through the friendships and check if there's a common language.
-        for (int i = 0; i < size; i++) {
-            vector<int> friends = friendships[i];
-            int first = friends[0];
-            int second = friends[1];
+        // Convert languages to set for easier lookups
+        vector<unordered_set<int>>langSet(m);
+        for (int i=0; i<m ;i++) {
+            langSet[i] = unordered_set<int>(languages[i].begin(), languages[i].end());
+        }      
 
-            vector<int> first_friend = languages[first - 1];
-            vector<int> second_friend = languages[second - 1];
-
-            int flag = 0;
-            for (int j = 0; j < first_friend.size(); j++) {
-                for (int k = 0; k < second_friend.size(); k++) {
-                    if (first_friend[j] == second_friend[k]) {
-                        flag = 1;
-                        break;
-                    }
+        // Find pairs thta cannot communicate 
+        unordered_set<int>cannotTalk;
+        for (auto& f: friendships) {
+            int u = f[0]-1;
+            int v = f[1]-1;
+            bool canCommunicate = false;
+            for (int lang: languages[u]) {
+                if (langSet[v].count(lang)) {
+                    canCommunicate = true;
+                    break;
                 }
             }
-            if (flag == 0) {
-                p.push_back({first, second});
-                mp[first]++;
-                mp[second]++;
+            if (!canCommunicate) {
+                cannotTalk.insert(u);
+                cannotTalk.insert(v);
             }
         }
 
-        // // Sort the pair vector based on the frequency of occurrences.
-        // sort(p.begin(), p.end(), [&](pair<int, int> a, pair<int, int> b) {
-        //     return mp[a.first] + mp[a.second] > mp[b.first] + mp[b.second];
-        // });
-
-        int minTeachings = INT_MAX;
-
-        for (int lang = 1; lang <= n; lang++) {
-            unordered_set<int> needTeach;
-            for (int i = 0; i < p.size(); i++) {
-                if (find(languages[p[i].first - 1].begin(), languages[p[i].first - 1].end(), lang) == languages[p[i].first - 1].end()) {
-                    needTeach.insert(p[i].first);
-                }
-                if (find(languages[p[i].second - 1].begin(), languages[p[i].second - 1].end(), lang) == languages[p[i].second - 1].end()) {
-                    needTeach.insert(p[i].second);
-                }
-            }
-            minTeachings = min(minTeachings, static_cast<int>(needTeach.size()));
+        // If all pairs can communicate , no teaching required
+        if (cannotTalk.empty()) {
+            return 0;
         }
 
-        return minTeachings;
+        // Count the frequency of each language among user who cannot communicate
+        unordered_map<int,int>freq;
+        for (int user: cannotTalk) {
+            for (int lang: languages[user]) {
+                freq[lang]++;
+            }
+        }
+
+        // Find the language with maximum frequency
+        int maxFreq = 0;
+        for (auto [lang,count]: freq) {
+            maxFreq = max(maxFreq, count);
+        }
+
+        return cannotTalk.size() - maxFreq;
     }
 };
