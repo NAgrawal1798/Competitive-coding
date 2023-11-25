c477dcb200626920ebbfc5cc7db6e4e9ba4c5f49
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-11-25 11:41:38 +0530

    Time: 3 ms (71.10%) | Memory: 10.1 MB (11.44%) - LeetSync

---
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    // Working solution
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        vector<int>firstCol;
+
+        for (int i=0; i<n; i++) {
+            firstCol.push_back(matrix[i][0]);
+        }
+
+        int rowIndex = upper_bound(firstCol.begin(), firstCol.end(), target) - firstCol.begin()-1;
+
+        if (rowIndex == -1) {
+            return false;
+        }
+
+        if (firstCol[rowIndex] == target) {
+            return true;
+        }
+
+
+
+        vector<int>row;
+
+        for (int j=0; j<m;j++) {
+            row.push_back(matrix[rowIndex][j]);
+        }
+        cout<<rowIndex<<endl;
+
+        int colIndex = upper_bound(row.begin(), row.end(), target) - row.begin()-1;
+
+        if (row[colIndex] == target) {
+            return true;
+        }
+
+        return false;
+
+    }
+};
