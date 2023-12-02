2d17e6bff740a51e0b31cc8779c32ab2b6ff4550
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-12-02 23:29:14 +0530

    Time: 27 ms (97.54%) | Memory: 20.8 MB (60.12%) - LeetSync

--- 971-shortest-bridge/shortest-bridge.cpp
+++ 971-shortest-bridge/shortest-bridge.cpp
@@ -1,49 +1,60 @@
 class Solution {
 public:
-    void dfs(int i,int j,vector<vector<int>>& grid,queue<pair<int,int>>& q){
-        if(i<0 || i>=grid.size() || j>=grid.size() || j<0 || grid[i][j]==-1 || grid[i][j]==0) return;
-        grid[i][j]=-1;
+
+    void dfs(vector<vector<int>>& grid, int i, int j, queue<pair<int,int>>&q) {
+        int n = grid.size();
+        int m = grid[0].size();
+        if (i < 0 || i>=n || j< 0 || j >= m || grid[i][j] == -1 || grid[i][j] == 0 ) {
+            return;
+        }
+        grid[i][j] = -1;
         q.push({i,j});
-        dfs(i,j-1,grid,q);
-        dfs(i,j+1,grid,q);
-        dfs(i-1,j,grid,q);
-        dfs(i+1,j,grid,q);
+        dfs(grid, i+1, j,q);
+        dfs(grid, i-1, j,q);
+        dfs(grid, i, j-1,q);
+        dfs(grid, i, j+1,q);
     }
     int shortestBridge(vector<vector<int>>& grid) {
-        int n=grid.size();
-        queue<pair<int,int>> q;
-        int b=true;
-        for(int i=0;i<n;i++){
-        if(b){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1){
-                    dfs(i,j,grid,q);
-                    b=false;
-                    break;
+        int n = grid.size();
+        int m = grid[0].size();
+        bool flag = true;
+        queue<pair<int,int>>q;
+
+        for (int i=0; i<n; i++) {
+            for (int j=0;j<m; j++) {
+                if (grid[i][j] == 1 && flag) {
+                    flag = false;
+                    dfs(grid, i, j, q);
                 }
             }
         }
-        }
-        int ans=0;
-        int r[]={-1,1,0,0};
-        int c[]={0,0,-1,1};
-        while(!q.empty()){
-            int a=q.size();
-            while(a--){
-                int i=q.front().first;
-                int j=q.front().second;
+        int ans= 0;
+        vector<int>dr = {-1,1,0,0};
+        vector<int>dc = {0,0,-1,1};
+
+        while(!q.empty()) {
+            int a = q.size();
+            while(a--) {
+                int i = q.front().first;
+                int j = q.front().second;
                 q.pop();
-                for(int k=0;k<4;k++){
-                    int nr=i+r[k];
-                    int nc=j+c[k];
-                    if(nr<0 || nr>=n || nc<0 || nc>=n || grid[nr][nc]==-1) continue;
-                    if(grid[nr][nc]==1) return ans;
-                    grid[nr][nc]=-1;
-                    q.push({nr,nc});
+                for (int k=0; k<4; k++) {
+                    int newr = i + dr[k];
+                    int newc = j + dc[k];
+                    if (newr < 0 || newr >= n || newc < 0 || newc >= m || grid[newr][newc] == -1) {
+                        continue;
+                    }
+                    if (grid[newr][newc] == 1) {
+                        return ans;
+                    }
+                    grid[newr][newc] = -1;
+                    q.push({newr, newc});
                 }
             }
             ans++;
         }
-        return 0;
+
+        return ans;
+
     }
 };
