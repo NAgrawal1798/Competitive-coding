ce489ba6c7a7293a1f6375f2493a46728e449323
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-12-02 11:45:43 +0530

    Time: 297 ms (27.75%) | Memory: 301.9 MB (8.61%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,30 +1,47 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-       int n = s.size();
-
-       vector<vector<bool>>dp(n, vector<bool>(n, false));
-
-       int ans = 0;
-       int start;
-
-       for (int iteration=0; iteration<n; iteration++) {
-           for (int j=0; j+iteration<n; j++) {
-               if (iteration==0) {
-                   dp[j][j] = true;
-               } else if (iteration==1 && s[j] == s[j+1]) {
-                   dp[j][j + 1] = true;
-               } else if (iteration > 1 && dp[j+1][j+iteration -1] && s[j] == s[j + iteration]) {
-                   dp[j][j + iteration] = true;
-               }
-
-               if (dp[j][j+iteration]) {
-                   ans = iteration;
-                   start = j;
-               }
-           }
-       }
-
-       return s.substr(start, ans+1);
+        int n = s.size();
+        string ans;
+        int maxi = 0;
+        
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        
+        // we have to traverse diagonals and above diagonals i.e.
+        // 0,0 -> 1,1 -> 2, 2-> 3, 3 ....
+        // 0,1 -> 1, 2 -> 2, 3 -> 3, 4 ...
+        // 0,2 -> 1, 3 -> 2, 4 -> 3,5 ...
+        for(int diff = 0; diff < n; diff++){
+            for(int i = 0, j = i + diff; j < n; i++, j++){
+                // diagonal elements are palindrome
+                if(i == j){
+                    dp[i][j] = 1;
+                }
+                // 2 digits are also palindrome having same i and j
+                else if(diff == 1){
+                    dp[i][j] = (s[i] == s[j]) ? 2 : 0;
+                }
+                else {
+                    // when outer strings are palindrome and inner strings are also palindrome then consider it as palindrome
+                    // aabaa
+                    // ^   ^
+                    // |   |
+                    // i. .j
+                    if(s[i] == s[j] && dp[i+1][j-1] > 0){
+                        dp[i][j] = 2 + dp[i+1][j-1];
+                    }
+                }
+                
+                if(dp[i][j]){
+                    if(maxi < j-i+1){
+                        // update longest palindrome
+                        maxi = j-i+1;
+                        ans = s.substr(i, maxi);
+                        // here i is starting point and maxi is length of substr
+                    }
+                }
+            }
+        }
+        return ans;
     }
 };
