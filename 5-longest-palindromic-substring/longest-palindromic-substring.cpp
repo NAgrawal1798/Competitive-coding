9b9ea280d11ddede7eff8ab93b8468b9776ea7ba
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-12-02 11:47:30 +0530

    Time: 852 ms (14.18%) | Memory: 23.3 MB (32.21%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,47 +1,39 @@
 class Solution {
+private: 
+    bool solve(vector<vector<bool>> &dp, int i, int j, string &s){
+        if(i == j){
+            return dp[i][j] = true;
+        }
+        if(j-i == 1){
+            if(s[i] == s[j]){
+                return dp[i][j] = true;
+            }
+            else{
+                return dp[i][j] = false;
+            }
+        }
+        if(s[i] == s[j] && dp[i+1][j-1] == true){
+            return dp[i][j] = true;
+        } else {
+            return dp[i][j] = false;
+        }
+    }
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        string ans;
-        int maxi = 0;
-        
-        vector<vector<int>> dp(n, vector<int>(n, 0));
-        
-        // we have to traverse diagonals and above diagonals i.e.
-        // 0,0 -> 1,1 -> 2, 2-> 3, 3 ....
-        // 0,1 -> 1, 2 -> 2, 3 -> 3, 4 ...
-        // 0,2 -> 1, 3 -> 2, 4 -> 3,5 ...
-        for(int diff = 0; diff < n; diff++){
-            for(int i = 0, j = i + diff; j < n; i++, j++){
-                // diagonal elements are palindrome
-                if(i == j){
-                    dp[i][j] = 1;
-                }
-                // 2 digits are also palindrome having same i and j
-                else if(diff == 1){
-                    dp[i][j] = (s[i] == s[j]) ? 2 : 0;
-                }
-                else {
-                    // when outer strings are palindrome and inner strings are also palindrome then consider it as palindrome
-                    // aabaa
-                    // ^   ^
-                    // |   |
-                    // i. .j
-                    if(s[i] == s[j] && dp[i+1][j-1] > 0){
-                        dp[i][j] = 2 + dp[i+1][j-1];
-                    }
-                }
-                
-                if(dp[i][j]){
-                    if(maxi < j-i+1){
-                        // update longest palindrome
-                        maxi = j-i+1;
-                        ans = s.substr(i, maxi);
-                        // here i is starting point and maxi is length of substr
+        int startIndex = 0; int maxlen = 0;
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        for(int g=0; g<n; g++){
+            for(int i=0, j=g; j<n; i++, j++){
+                solve(dp, i, j, s);
+                if(dp[i][j] == true){
+                    if(j-i+1 > maxlen){
+                        startIndex = i;
+                        maxlen = j-i+1;
                     }
                 }
             }
         }
-        return ans;
+        return s.substr(startIndex, maxlen);
     }
 };
