8084ef1f3855067c83c94931fb1067ddd6561b93
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-12-09 11:56:41 +0530

    Time: 17 ms (64.02%) | Memory: 13.8 MB (84.78%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.cpp
+++ 210-course-schedule-ii/course-schedule-ii.cpp
@@ -1,72 +1,43 @@
 class Solution {
 public:
-    // Make a graph
-    // Appy dfs or bfs to count the nodes
-
-    // void dfs(vector<vector<int>>& adj, stack<int>&s, int node, vector<int>&visited) {
-    //     visited[node] = 1;
-
-    //     for (auto it: adj[node]) {
-    //         if (!visited[it]) {
-    //             dfs(adj, s, it, visited);
-    //         } 
-    //     }
-
-    //     s.push(node);
-    // }
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         vector<int>ans;
+
         vector<int>indegree(numCourses, 0);
-        queue<int>q;
         vector<vector<int>>adj(numCourses);
-
-        for (int i=0;i<prerequisites.size(); i++) {
-            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        for (int i=0; i<prerequisites.size(); i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            indegree[u]++;
+            adj[v].push_back(u);
         }
 
-        for (int i=0;i<numCourses; i++) {
-            for (auto it: adj[i]) {
-                indegree[it]++;
-            }
-        }
+        queue<int>q;
 
         for (int i=0; i<numCourses; i++) {
             if (indegree[i] == 0) {
                 q.push(i);
+                ans.push_back(i);
             }
         }
 
         while(!q.empty()) {
             int node = q.front();
-            ans.push_back(node);
             q.pop();
 
-            for (auto it: adj[node]) {
+            for (auto it : adj[node]) {
                 indegree[it]--;
                 if (indegree[it] == 0) {
                     q.push(it);
+                    ans.push_back(it);
                 }
             }
         }
 
-        if (ans.size() == numCourses) {
-            return ans;
+        if (ans.size() != numCourses) {
+            return vector<int>{};
         }
 
-        return {};
-
-
-
-        // for (int i=0; i<numCourses; i++) {
-        //     if (!visited[i]) {
-        //         dfs(adj, s, i, visited);
-        //     }
-        // }
-
-        // while(!s.empty()) {
-        //     ans.push_back(s.top());
-        //     s.pop();
-        // }
         return ans;
     }
 };
