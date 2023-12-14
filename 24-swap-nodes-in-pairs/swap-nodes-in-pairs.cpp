d177c4bf210c8325cc5feaea772af2820f01c7f5
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-12-15 00:05:57 +0530

    Time: 4 ms (41.22%) | Memory: 7.8 MB (56.02%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool check(ListNode* head, int k) {
+        while(k--) {
+            if (head == NULL) {
+                return false;
+            }
+            head = head->next;
+        }
+        return true;
+    }
+    ListNode* reverseK(ListNode* head, int k) {
+        if (head == NULL || !check(head, k)) {
+            return head;
+        }
+
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next1 = NULL;
+        ListNode* first = head;
+
+        int count=0;
+        while(count++ < k) {
+            next1 = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr=next1;
+        }
+        first->next = reverseK(curr, k);
+        return prev;
+
+    }
+    ListNode* swapPairs(ListNode* head) {
+        return reverseK(head, 2);
+    }
+};
