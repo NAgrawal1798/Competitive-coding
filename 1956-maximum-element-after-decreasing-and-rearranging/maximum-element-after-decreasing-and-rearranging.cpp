c5d9ab12e142244d4f26760a69849a7704bc2916
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2023-12-18 23:33:14 +0530

    Time: 82 ms (38.05%) | Memory: 51.7 MB (20.08%) - LeetSync

---
+++ 1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    // arr
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        sort(arr.begin(), arr.end());
+
+        int ans = 1;
+        int n = arr.size();
+        arr[0]= 1;
+
+        for (int i=0; i<n-1; i++) {
+            int diff = arr[i+1] - arr[i];
+            if (diff <= 1) {
+                ans = max(ans, arr[i+1]);
+            } else {
+                arr[i+1] = arr[i] + 1;
+                ans = max(ans, arr[i+1]);
+            }
+        }
+
+        return ans;
+    }
+};
