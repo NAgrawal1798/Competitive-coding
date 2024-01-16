453a69ef14adee49ca9e30d61253938ab0098faf
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-01-16 20:23:22 +0530

    Time: 31 ms (54.22%) | Memory: 13.5 MB (25.29%) - LeetSync

---
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    // Use stack to get - O(n) soln
+    int minInsertions(string s) {
+        int minInsert = 0;
+        stack<char>st;
+        int n = s.length();
+        for (int i=0; i<n; i++) {
+            if (s[i] == '(') {
+                st.push('(');
+            } else { // Add condition to check the count
+                if ( s[i] == ')' && i < n && s[i + 1] == ')') {
+                    if (!st.empty()) {
+                        st.pop();
+                    } else {
+                        minInsert++;
+                    }
+                    i++;
+                } else if (s[i] == ')' && i < n && s[i + 1] != ')' ){
+                    if (!st.empty()) {
+                        st.pop();
+                        minInsert++;
+                    } else {
+                        minInsert += 2;
+                    }
+                }
+            }
+        }
+
+        if(st.empty()) {
+            return minInsert;
+        }
+        else {
+            return minInsert + 2*st.size();
+        }
+    }
+};
