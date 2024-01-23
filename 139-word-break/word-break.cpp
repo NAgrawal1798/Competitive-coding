61e18f18c071cc6c5961ad14a56c1f4783d4fcae
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-01-23 23:13:13 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (55.84%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,37 +1,23 @@
 class Solution {
 public:
-    bool helper(string s, set<string>&present, map<string, bool>&mp) {
-        
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.length();
+        vector<bool> m(s.length()+1, false);
+        m[0] = true;
 
-        if (present.count(s)) {
-            return true;
-        }
-        if (mp.find(s) != mp.end()) {
-            return mp[s];
-        }
-
-        for (int i=1; i<s.length(); i++) {
-            string left = s.substr(0,i);
-            if (present.count(left) && helper(s.substr(i), present, mp)) {
-                mp[left] = true;
-                return true;
+        for(int i=0; i<s.length(); ++i) {
+            if(m[i]==true) {
+                for(auto word : wordDict) {
+                    if(i+word.length()-1<n) {
+                        if(word==s.substr(i, word.length())) {
+                            m[i+word.length()] = true;
+                        }
+                    }
+                }
             }
         }
 
-        return mp[s] = false;
 
-
-    }
-
-    bool wordBreak(string s, vector<string>& wordDict) {
-        set<string>present;
-
-        for (int i=0;i<wordDict.size(); i++) {
-            present.insert(wordDict[i]);
-        }
-
-        
-        map<string,bool>mp;
-        return helper(s,present, mp);
+        return m[s.length()];
     }
 };
