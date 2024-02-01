3a97808b388767e1fac7c930e828b0d87fe52ea4
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-02-01 23:10:22 +0530

    Time: 36 ms (94.21%) | Memory: 10.6 MB (27.81%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int helper(int n) {
+        if(n==1) {
+            return n; 
+        }
+        vector<int>dp(n+1,INT_MAX);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j*j<=i; j++) {
+                dp[i] = min(dp[i], dp[i-j*j] + 1);
+            }
+        }
+        return dp[n];
+    }
+
+    int numSquares(int n) {
+        return helper(n);
+    }
+};
