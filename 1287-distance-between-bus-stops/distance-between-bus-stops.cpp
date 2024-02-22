dc723c34d9c123ad7b93300a81565f6bb7cec7aa
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-02-22 21:12:24 +0530

    Time: 3 ms (64.40%) | Memory: 11.2 MB (6.02%) - LeetSync

--- 1287-distance-between-bus-stops/distance-between-bus-stops.cpp
+++ 1287-distance-between-bus-stops/distance-between-bus-stops.cpp
@@ -1,34 +1,15 @@
 class Solution {
 public:
     int distanceBetweenBusStops(vector<int>& distance, int start, int destination) {
-        // apply dfs and bfs
-
-        // we need to write the generic code
-
-        int n = distance.size();
-        int ans1 = 0;
-        int start1 = start;
-        int destination1 = destination;
-        while(start1  != destination1) {
-            ans1 += distance[start1];
-            start1++;
-            if(start1 > n-1) {
-                start1 = 0;
-            }
-        }
-
-        int ans2=0;
-        int start2 = start;
-        int destination2 = destination;
-        while(destination2 != start2) {
-            ans2 += distance[destination2];
-            destination2++;
-            if(destination2 > n-1) {
-                destination2 = 0;
-            }
-        }
-
-
-        return min(ans1, ans2);
+        int s = min(start,destination);
+        int d = max(start,destination);
+        int l = distance.size();
+        int dt[2*l];
+        for(int i=0;i<l;i++){dt[i]=distance[i];dt[i+l]=distance[i];}
+        int s1 = 0;
+        int s2 = 0;
+        for(int i=s;i<d;i++){s1+=dt[i];}
+        for(int i=d;i<s+l;i++){s2+=dt[i];}
+        return min(s1,s2);
     }
 };
