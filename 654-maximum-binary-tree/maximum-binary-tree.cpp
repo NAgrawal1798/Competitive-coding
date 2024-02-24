4ad20db12487e10fa02449219793b56b78dea9cc
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-02-24 17:03:51 +0530

    Time: 65 ms (41.24%) | Memory: 41.6 MB (73.29%) - LeetSync

--- 654-maximum-binary-tree/maximum-binary-tree.cpp
+++ 654-maximum-binary-tree/maximum-binary-tree.cpp
@@ -11,28 +11,31 @@
  */
 class Solution {
 public:
-
-    int findMaxValue(vector<int>& nums, int l, int r) {
-        int max_i = l;
-        for (int i = l; i < r; i++) {
-            if (nums[max_i] < nums[i])
-                max_i = i;
-        }
-        return max_i;
+    //  solve using recursion
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        int n = nums.size();
+        return helper(nums, 0, n);
     }
 
-    TreeNode* construct(vector<int>& nums, int left, int right) {
-        if (left == right) {
+    TreeNode* helper(vector<int>& nums, int l, int r) {
+        if(l==r) {
             return NULL;
         }
-        int max_i = findMaxValue(nums, left, right);
-        TreeNode* root = new TreeNode(nums[max_i]);
-        root->left = construct(nums, left, max_i);
-        root->right = construct(nums, max_i + 1, right);
-        return root;
-
+        int maxi = findMaxi(nums, l, r);
+        TreeNode* node = new TreeNode(nums[maxi]);
+        node->left = helper(nums, l, maxi);
+        node->right = helper(nums, maxi+1, r);
+        return node;
     }
-    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        return construct(nums, 0, nums.size());
+
+    int findMaxi(vector<int>& nums, int l, int r) {
+        int maxi = l;
+        for(int i=l; i<r; i++) {
+            if(nums[maxi] < nums[i]) {
+                maxi = i;
+            }
+        }
+
+        return maxi;
     }
 };
