86f23d72b3d40b594faa28aadf912ae649a0fb28
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-03-30 01:39:35 +0530

    Time: 4 ms (20.31%) | Memory: 7.9 MB (53.69%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -1,31 +1,26 @@
-// int speedup = []{ios::sync_with_stdio(0); cin.tie(0); return 0;}();
 class Solution {
 public:
-// this function will return number of combinations to achieve sum==left...
-    int get_combination(int left,vector<int>&nums,vector<int>&dp){
-        if(left==0){
-            // some how we have achieved our sum==target there left target==0
-            return 1;
-        }
-        if(left<0){
+    // without dp
+    int helper(vector<int>& nums, int target, vector<int>& dp) {
+        // base cases
+        if(target < 0) {
             return 0;
         }
-        if(dp[left]!=-1){
-            // we know number of combinations corresponding to left..
-            return dp[left];
+        if(target == 0) {
+            return 1;
         }
-        int ans=0;
-        for(auto ele:nums){
-            // if we have ele.. left will be decresed by ele..
-            ans+=get_combination(left-ele,nums,dp);
+        if(dp[target] != -1) {
+            return dp[target];
         }
-        return dp[left]=ans;
+        int ans = 0;
+        for(int i=0; i<nums.size(); i++) {
+            ans += helper(nums, target - nums[i], dp);
+        }
+        return dp[target] = ans;
     }
     int combinationSum4(vector<int>& nums, int target) {
-        vector<int>dp(target+1,-1); // it will store number of combinations corresponding to each sum from 0-target...
-
-        // initially left==target...
-        return get_combination(target,nums,dp);
-
+        // use dp
+        vector<int>dp(target+1, -1);
+        return helper(nums, target, dp);
     }
 };
