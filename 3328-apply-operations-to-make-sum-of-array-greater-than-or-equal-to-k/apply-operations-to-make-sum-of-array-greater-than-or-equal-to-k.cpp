ff92522436be2b61892e83438fd87ec52d75ebee
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-03-30 10:31:02 +0530

    Time: 7 ms (13.34%) | Memory: 7.4 MB (10.98%) - LeetSync

---
+++ 3328-apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k/apply-operations-to-make-sum-of-array-greater-than-or-equal-to-k.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int minOperations(int k) {
+        if(k==1) {
+            return 0;
+        }
+        int ans = k;
+        for(int i=1; i<=k; i++) {
+            int t = (i-1) + (ceil((double)k/(double)i) - 1);
+            ans = min(ans, t);
+        }
+        return ans;
+    }
+};
