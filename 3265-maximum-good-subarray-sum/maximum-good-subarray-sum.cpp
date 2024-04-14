68fc30c3bfa3381354b2ca192e8084538709830d
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-04-14 13:39:46 +0530

    Time: 285 ms (44.39%) | Memory: 151.5 MB (14.83%) - LeetSync

--- 3265-maximum-good-subarray-sum/maximum-good-subarray-sum.cpp
+++ 3265-maximum-good-subarray-sum/maximum-good-subarray-sum.cpp
@@ -1,77 +1,25 @@
 class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
-    //   int n = nums.size();
-
-    //   unordered_map<int, vector<int>>mp;
-
-    //   vector<long long>sum(n);
-
-    //   for(int i=0; i<n; i++) {
-    //     mp[nums[i]].push_back(i);
-    //     if(i==0) {
-    //         sum[i] = nums[i];
-    //     } else {
-    //         sum[i] = nums[i] + sum[i-1];
-    //     }
-    //   }
-
-    //   double mx = LONG_MIN + 100;
-    //   for(int i=0; i<n; i++) {
-    //     int target = nums[i] + k;
-
-    //     for(auto ind: mp[target]) {
-    //         if(ind > i) {
-    //             mx = max(mx, (double)sum[ind] - sum[i] + nums[i]);
-    //         }
-    //     }
-
-
-    //     target = nums[i] - k;
-    //     for(auto ind: mp[target]) {
-    //         if(ind > i) {
-    //             mx = max(mx, (double)sum[ind] - sum[i] + nums[i]);
-    //         }
-    //     }
-    //   }
-    //   if(mx == LONG_MIN + 100) {
-    //     return 0;
-    //   }
-    //   return mx;
-    // }
-
-    int n = nums.size();
-    unordered_map<long long, long long>mp;
-    vector<long long>sum(n);
-
-    sum[0] = nums[0];
-    mp[nums[0]] = sum[0];
-
-    long long ans = LLONG_MIN;
-
-    for(int i=1; i<n; i++) {
-        // prefix sum
-        sum[i] = sum[i-1] + nums[i];
-
-        auto it = mp.find(nums[i]);
-        if(it != mp.end()) {
-            mp[nums[i]] = min(sum[i], mp[nums[i]]);
-        } else {
-            mp[nums[i]] = sum[i];
+        int n = nums.size();
+        long ans = LONG_MIN, preSum = 0;
+        unordered_map<int, int> mp;
+        vector<long> pre;
+        for(auto it : nums){
+            preSum += it;
+            pre.push_back(preSum);
         }
-
-        // target 1 = nums[i] + k
-        it = mp.find(nums[i] + k);
-        if(it!= mp.end()) {
-            ans = max(ans, sum[i] - mp[nums[i] + k] + nums[i] + k);
+        for(int i = 0 ; i < n ; i++){
+            int diff1 = nums[i] - k, diff2 = nums[i] + k;
+            if(mp.contains(diff1))
+                ans = max(ans, pre[i] - pre[mp[diff1]] + diff1);
+            if(mp.contains(diff2))
+                ans = max(ans, pre[i] - pre[mp[diff2]] + diff2);
+            if(mp.contains(nums[i]))
+                mp[nums[i]] = pre[i] < pre[mp[nums[i]]] ? i : mp[nums[i]];
+            else
+                mp[nums[i]] = i;
         }
-
-        it = mp.find(nums[i] - k);
-        if(it != mp.end()) ans = max(ans, sum[i] - mp[nums[i] - k] + nums[i] - k);
-    }
-
-    if(ans == LLONG_MIN) return 0;
-    return ans;
-
+        return ans != LONG_MIN ? ans : 0;
     }
 };
