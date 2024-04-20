92ad3bbb96d3495df9fcd557c568a956a9e0be1b
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-04-20 17:48:54 +0530

    Time: 12 ms (13.33%) | Memory: 14.3 MB (5.50%) - LeetSync

--- 414-third-maximum-number/third-maximum-number.cpp
+++ 414-third-maximum-number/third-maximum-number.cpp
@@ -2,41 +2,29 @@ class Solution {
 public:
     // Solve this question using priority_queue
     int thirdMax(vector<int>& nums) {
-        // max_heap datastructure
-        priority_queue<int, vector<int>, greater<int>>minHeap; // Return min element
-
-        unordered_set<int>taken;
-
-        for (int i=0; i<nums.size(); i++) {
-            if (taken.count(nums[i])) {
+        priority_queue<int> pq;
+        int n = nums.size();
+        set<int>s;
+        for(int i=0; i<n; i++) {
+            if(s.count(nums[i]) > 0) {
                 continue;
-            }
-
-            if (minHeap.size() == 3) {
-                // If the element is greater then the top most element
-
-                int x = minHeap.top();
-                if (x < nums[i]) {
-                    minHeap.pop();
-                    taken.erase(x);
-                    minHeap.push(nums[i]);
-                    taken.insert(nums[i]);
-                }
             } else {
-                minHeap.push(nums[i]);
-                taken.insert(nums[i]);
+                pq.push(nums[i]);
+                s.insert(nums[i]);
             }
         }
 
-        // nums has only one distinct element
-        if (minHeap.size() == 1) {
-            return minHeap.top();
-        } else if (minHeap.size() == 2) {
-            int firstNum = minHeap.top();
-            minHeap.pop();
-            return minHeap.top();
+        if(pq.size() < 3) {
+            int ans = pq.top();
+            return ans;
+        }
+        int k=3;
+        while(k>1 ) {
+            pq.pop();
+            k--;
         }
 
-        return minHeap.top();
+        int ans = pq.top();
+        return ans;
     }
 };
