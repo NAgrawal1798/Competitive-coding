fe99d786e3919f7c7eae6539146b5f37a489fcd4
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-04-20 17:33:01 +0530

    Time: 130 ms (91.69%) | Memory: 131.5 MB (18.56%) - LeetSync

---
+++ 204-count-primes/count-primes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+        // use seive method
+        if(n<=2) {
+            return 0;
+        }
+
+        int count = 0;
+        vector<int>v(n+1, 0);
+        for(int i=2; i*i<=n; i++) {
+            if(v[i] == 0) {
+                for(int j=i*i; j<=n; j+=i){
+                    v[j] = 1;
+                }
+            }
+        }
+
+        for(int i=2; i<n; i++) {
+            if(v[i] == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
