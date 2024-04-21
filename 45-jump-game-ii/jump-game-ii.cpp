cb2dc8e40224c51d64d55844023ce3e1427a599a
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-04-21 22:33:09 +0530

    Time: 66 ms (27.10%) | Memory: 19.9 MB (11.21%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -2,24 +2,14 @@ class Solution {
 public:
     // dp solution
     int jump(vector<int>& nums) {
-
         int n = nums.size();
-        vector<int>jumps(n, INT_MAX);
-
+        vector<int>jumps(n+1000, INT_MAX);
         jumps[0] = 0;
-
-        int i=0;
-        while(i < n) {
-            int num = nums[i];
-            int k=1;
-
-            while(k <= num && i+k < n) {
-                if (jumps[i+k] > jumps[i] + 1) {
-                    jumps[i+k] = jumps[i] + 1;
-                }
-                k++;
+        for(int i=0; i<n; i++) {
+            int dist = nums[i];
+            for (int j = 1; j <= dist && i + j < n; j++) {
+                jumps[i + j] = min(jumps[i] + 1, jumps[i + j]);
             }
-            i++;
         }
 
         return jumps[n-1];
