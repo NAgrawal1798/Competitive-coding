44252de415d30980ef00eade29319582fd2b6cdd
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-04-24 08:14:42 +0530

    Time: 173 ms (61.63%) | Memory: 136.9 MB (89.93%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        // I want a data structure that can pop the elements
-        // from front and push it from the back
-        // reason to pick deque
         deque<int>dq;
         vector<int>res;
+
         for(int i=0; i<nums.size(); i++) {
-            // before it remove the small elements
             if(dq.front() == i-k) {
                 dq.pop_front();
             }
-            // we are storing indices in dq
-            // while nums[last_indice_stored] < nums[i], we will have to pop the last element
             while(!dq.empty() && nums[dq.back()] < nums[i]) {
                 dq.pop_back();
             }
-            dq.push_back(i); // pushing index
-            if( i>= k-1) {
+            dq.push_back(i);
+            if(i >= k-1) {
                 res.push_back(nums[dq.front()]);
             }
         }
