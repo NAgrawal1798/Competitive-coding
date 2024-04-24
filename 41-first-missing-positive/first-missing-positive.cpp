754f924256af33c34ffc8713c59fdca6768015c6
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-04-24 09:07:55 +0530

    Time: 60 ms (22.42%) | Memory: 53.3 MB (21.26%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.cpp
+++ 41-first-missing-positive/first-missing-positive.cpp
@@ -4,26 +4,18 @@ public:
         int n = nums.size();
         int i=0;
         while(i<n) {
-            if (nums[i] > 0 && nums[i] <= n && nums[nums[i]-1] != nums[i]) {
-                swap(nums[i], nums[nums[i] - 1]);
+            if(nums[i] > 0 && nums[i] <= n && nums[nums[i] -1] != nums[i]) {
+                swap(nums[i], nums[nums[i]  - 1]);
             } else {
                 i++;
             }
         }
-        // for (int i=0; i<n; i++) {
-        //     if (nums[i] > 0 && nums[i] <= n) {
-                
-        //     }
-        // }
 
-        int ans = 0;
-        cout<<n<<endl;
-
-        for (int i=0; i<n; i++) {
-            // cout<<nums[i]<<endl;
-            if (nums[i] != i+1) {
+        // check if the number is in the correct position
+        for(int i=0; i<n; i++) {
+            if(nums[i] != i+1) {
                 return i+1;
-            }
+            } 
         }
 
         return n+1;
