e4f193f11fa218850a6ed50034376b870e80a555
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-04-30 11:09:08 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (59.26%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -24,11 +24,10 @@ public:
     int findMin(vector<int>& nums) {
         int n = nums.size();
         int pivot = findPivot(nums, 0, n - 1);
-        cout<<pivot<<endl;
  
         // If we didn't find a pivot,
         // then array is not rotated at all
-        if (pivot == -1 || pivot == n-1) {
+        if (pivot == n-1) {
             return nums[0];
         }
         if(n == 1) {
