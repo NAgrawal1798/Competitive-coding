5a7d8ebc96e265ab6f8d62203fdf0c23d63a34d2
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-05-03 12:20:36 +0530

    Time: 570 ms (17.23%) | Memory: 26.5 MB (32.61%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,40 +1,35 @@
 class Solution {
 public:
-    bool solve(vector<vector<bool>>&dp, int i, int j, string &s) {
-        if (i==j) {
-            return dp[i][j] = true;
-        }
-        if (j-i==1) {
+    void solve(vector<vector<bool>>&dp, int i, int j, string &s) {
+        if(i==j) {
+            dp[i][j] = true;
+        } else if(j-i == 1) {
+            if(s[i] == s[j]) {
+                dp[i][j] = true;
+            }
+        } else {
             if (s[i] == s[j]) {
-                return dp[i][j] = true;
-            } else {
-                return dp[i][j] = false;
+                dp[i][j] = dp[i+1][j-1];
             }
         }
-        if (s[i] == s[j] && dp[i+1][j-1] == true) {
-            return dp[i][j] = true;
-        } else {
-            return dp[i][j] = false;
-        }
     }
-    string longestPalindrome(string s) {
-        int n = s.size();
-        int startIndex = 0;
-        int maxLen = 0;
-        vector<vector<bool>> dp(n, vector<bool>(n, false));
-
-        for (int g=0; g<n; g++) {
-            for (int i=0, j=g; j<n; i++, j++) {
+    std::string longestPalindrome(std::string s) {
+       int n = s.size();
+        vector<vector<bool>>dp(n, vector<bool>(n, false));
+        int maxi = 0;
+        int ans_i =0;
+        for(int g=0; g<n; g++) {
+            for(int i=0, j=g; j<n; i++,j++) {
                 solve(dp, i, j, s);
-                if (dp[i][j] == true) {
-                    if (j-i+1 > maxLen) {
-                        startIndex = i;
-                        maxLen = j-i+1;
+                if (dp[i][j]) {
+                    if (maxi < (j-i+1)) {
+                        maxi = j-i+1;
+                        ans_i = i;
                     }
                 }
             }
         }
 
-        return s.substr(startIndex, maxLen);
+        return s.substr(ans_i, maxi);
     }
 };
