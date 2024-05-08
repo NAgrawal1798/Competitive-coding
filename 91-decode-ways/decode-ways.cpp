139d25e67db6eca47ae25807020e728bea7ea9b3
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-05-08 08:49:37 +0530

    Time: 34 ms (5.32%) | Memory: 20.8 MB (5.03%) - LeetSync

---
+++ 91-decode-ways/decode-ways.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        if (s[0] == '0') {
+            return false;
+        }
+        if (s.size() > 2) {
+            return false;
+        }
+        int num = stoi(s);
+        return num >= 1 && num <= 26;
+    }
+
+    int helper(string &s, int index, vector<int>& dp) {
+        if (index == s.size()) {
+            return 1; // Reached the end, one valid decoding
+        }
+        if (dp[index] != -1) {
+            return dp[index]; // Return the value if it's already calculated
+        }
+
+        int ways = 0;
+        for (int i = index + 1; i <= s.size(); i++) {
+            if (isValid(s.substr(index, i - index))) {
+                ways += helper(s, i, dp);
+            }
+        }
+
+        dp[index] = ways; // Store the result in the DP array
+        return ways;
+    }
+
+    int numDecodings(string s) {
+        vector<int> dp(s.size(), -1); // Initialize DP array with -1
+        return helper(s, 0, dp);
+    }
+};
