4b0f40c036e9ea33eafc4deaee33c65f8910842e
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-05-09 09:10:32 +0530

    Time: 18 ms (28.39%) | Memory: 18 MB (23.12%) - LeetSync

--- 139-word-break/word-break.cpp
+++ 139-word-break/word-break.cpp
@@ -1,23 +1,31 @@
 class Solution {
 public:
-    bool wordBreak(string s, vector<string>& wordDict) {
-        int n = s.length();
-        vector<bool> m(s.length()+1, false);
-        m[0] = true;
+    set<string>st;
+    bool helper(string s, int index, vector<int> &dp) {
+        // base case
+        if(index == s.size()) {
+            return true;
+        }
+        if (dp[index] != -1) {
+            return dp[index];
+        }
 
-        for(int i=0; i<s.length(); ++i) {
-            if(m[i]==true) {
-                for(auto word : wordDict) {
-                    if(i+word.length()-1<n) {
-                        if(word==s.substr(i, word.length())) {
-                            m[i+word.length()] = true;
-                        }
-                    }
+        for(int i=index; i<s.size(); i++) {
+            if(st.count(s.substr(index, i - index + 1))) {
+                if(helper(s, i + 1, dp)) {
+                    return dp[index] = 1;
                 }
             }
         }
 
-
-        return m[s.length()];
+        return dp[index] = 0;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = wordDict.size();
+        vector<int>dp(s.size(), -1);
+        for(int i=0; i<n; i++) {
+            st.insert(wordDict[i]);
+        }
+        return helper(s, 0, dp);
     }
 };
