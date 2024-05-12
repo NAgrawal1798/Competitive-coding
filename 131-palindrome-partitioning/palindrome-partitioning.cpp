52956ac30cb55a60679ec42816d151630f5d2f87
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-05-12 13:53:47 +0530

    Time: 115 ms (28.62%) | Memory: 61 MB (58.21%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
-    // dp solution kind of backtracking
-    // cut it or not cut it
-    // Its more like to find the all possible subset which is 
-    // being done by the backtracking
-    bool check(int i, int j, string s) {
+    bool check(string s, int i, int j) {
         while(i<=j) {
             if (s[i] != s[j]) {
                 return false;
@@ -13,24 +9,26 @@ public:
             j--;
         }
         return true;
-    } 
-    void helper(string s , int index, vector<vector<string>>&ans, vector<string>&path) {
-        if (index == s.length()) {
+    }
+    void helper(string s, int index,vector<string>& path, vector<vector<string>>&ans) {
+        if (index == s.size()) {
             ans.push_back(path);
             return;
         }
-        for (int i = index; i< s.length(); i++) {
-            if (check(index, i, s)) {
-                path.push_back(s.substr(index,i-index+1));
-                helper(s, i + 1, ans, path);
+        for(int i=index; i<s.size(); i++) {
+            if (check(s, index, i)) {
+                path.push_back(s.substr(index, i-index + 1));
+                helper(s, i+1, path, ans);
                 path.pop_back();
             }
         }
+
+        return;
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>>ans;
         vector<string>path;
-        helper(s, 0, ans, path);
+        helper(s, 0, path, ans);
         return ans;
     }
 };
