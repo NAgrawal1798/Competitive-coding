b9688b168d968d1c341924733459f86b6ecfbd41
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-05-13 08:04:15 +0530

    Time: 11 ms (53.79%) | Memory: 13.7 MB (33.23%) - LeetSync

--- 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.cpp
+++ 2454-largest-local-values-in-a-matrix/largest-local-values-in-a-matrix.cpp
@@ -7,9 +7,15 @@ public:
         
         for(int i=1; i<n-1; i++) {
             for(int j=1; j<n-1; j++) {
-                int maxi = max({grid[i][j], grid[i-1][j], grid[i+1][j],
-                        grid[i-1][j-1], grid[i+1][j+1], grid[i][j-1],
-                        grid[i][j+1], grid[i-1][j+1], grid[i+1][j-1]});
+                int maxi = 0;
+                for(int k=-1; k<=1; k++) {
+                    for(int l=-1; l<=1; l++) {
+                        maxi = max(maxi, grid[i+k][j+l]);
+                    }
+                }
+                // int maxi = max({grid[i][j], grid[i-1][j], grid[i+1][j],
+                //         grid[i-1][j-1], grid[i+1][j+1], grid[i][j-1],
+                //         grid[i][j+1], grid[i-1][j+1], grid[i+1][j-1]});
                 ans[i-1][j-1] = maxi;
             }
         }
