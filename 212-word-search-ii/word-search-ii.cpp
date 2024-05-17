33518009372aa3d429cdee8c0e2671c2c35bfcd9
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-05-17 06:32:35 +0530

    Time: 1367 ms (32.45%) | Memory: 21.4 MB (20.01%) - LeetSync

--- 212-word-search-ii/word-search-ii.cpp
+++ 212-word-search-ii/word-search-ii.cpp
@@ -1,14 +1,19 @@
-class TrieNode {
-    public:
-        TrieNode* children[26];
-        bool isWord;
+#include <vector>
+#include <string>
 
-        TrieNode() {
-            for(int i=0; i<26; i++) {
-                children[i] = NULL;
-            }
-            isWord = false;
+using namespace std;
+
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isWord;
+
+    TrieNode() {
+        for(int i = 0; i < 26; i++) {
+            children[i] = NULL;
         }
+        isWord = false;
+    }
 };
 
 class Solution {
@@ -18,7 +23,7 @@ private:
     void insert(string word) {
         TrieNode* node = root; // start from the root
 
-        for(int i=0; i<word.size(); i++) {
+        for(int i = 0; i < word.size(); i++) {
             int curr = word[i] - 'a';
             if(node->children[curr] == NULL) {
                 node->children[curr] = new TrieNode();
@@ -28,48 +33,50 @@ private:
         node->isWord = true;
     }
 
-    // Method to perform dfs search
-    void search(vector<vector<char>>& board, int i, int j, int n, int m, TrieNode* node, string word, vector<string>& result) {
-        if(i<0 || i>=n || j<0 || j>=m || board[i][j] == '#') {
+    // Method to perform DFS search
+    void search(vector<vector<char>>& board, int i, int j, int n, int m, TrieNode* node, string word, vector<string>& result,
+                vector<vector<int>>& visited) {
+        if(i < 0 || i >= n || j < 0 || j >= m || visited[i][j]) {
             return; // Return if cell is out of bound or already visited
         }
 
-        char c=board[i][j];
-
+        char c = board[i][j];
         node = node->children[c - 'a'];
-        if (node ==NULL) {
+        if (node == NULL) {
             return;
         }
 
         word += c;
         if (node->isWord) {
             result.push_back(word);
-            node->isWord = false;
+            node->isWord = false; // Prevent duplicate entries
         }
-        board[i][j]='#';
-        search(board, i - 1, j, n, m, node, word, result);
-        search(board, i + 1, j, n, m, node, word, result);
-        search(board, i, j - 1, n, m, node, word, result);
-        search(board, i, j + 1, n, m, node, word, result);
-        board[i][j] = c;
-        }
+        visited[i][j] = 1;
+
+        search(board, i - 1, j, n, m, node, word, result, visited);
+        search(board, i + 1, j, n, m, node, word, result, visited);
+        search(board, i, j - 1, n, m, node, word, result, visited);
+        search(board, i, j + 1, n, m, node, word, result, visited);
+
+        visited[i][j] = 0; // Backtrack
+    }
 
 public:
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
         // Trie implementation to efficiently search words on the board
-        for(int i=0; i<words.size(); i++) {
+        for(int i = 0; i < words.size(); i++) {
             insert(words[i]);
         }
 
         int n = board.size();
         int m = board[0].size();
 
-        TrieNode* node = root;
-        vector<string>result;
+        vector<string> result;
 
-        for(int i=0; i<n; i++) {
-            for(int j=0;j<m; j++) {
-                search(board, i, j, n, m, node, "", result);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                vector<vector<int>> visited(n, vector<int>(m, 0));
+                search(board, i, j, n, m, root, "", result, visited);
             }
         }
 
