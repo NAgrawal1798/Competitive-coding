c30e665489afc0398a2efc861fded242f6d5c240
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-06-07 08:08:12 +0530

    Time: 18 ms (88.73%) | Memory: 24.4 MB (9.57%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,20 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.size() == 0) {
+        if(intervals.size() == 1) {
             return intervals;
         }
-        sort(intervals.begin(), intervals.end());
-        vector<vector<int>>ans;
+
+        vector<pair<int,int>>p;
         for(int i=0; i<intervals.size(); i++) {
-            // if ans is empty and there is no overlapping
-            if(ans.size() == 0 || ans.back()[1] < intervals[i][0]) {
-                ans.push_back(intervals[i]);
+            p.push_back({intervals[i][0], intervals[i][1]});
+        }
+        sort(p.begin(), p.end());
+
+        vector<vector<int>>ans;
+
+        int f = p[0].first;
+        int s = p[0].second;
+
+        for(int i=0; i<p.size() - 1; i++) {
+            vector<int>a(2);
+            if (s >= p[i+1].first) {
+                s = max(p[i+1].second, s);
             } else {
-                ans.back()[1] = max(ans.back()[1] , intervals[i][1]);
+                a[0] = f;
+                a[1] = s;
+                f = p[i+1].first;
+                s = p[i+1].second;
+                ans.push_back(a);
             }
         }
 
+        ans.push_back({f, s});
         return ans;
     }
 };
