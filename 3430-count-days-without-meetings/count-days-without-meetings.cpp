b2abf4477590bf065d61db7bd6e7079e63cd111f
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-06-09 10:11:17 +0530

    Time: 307 ms (87.99%) | Memory: 125.6 MB (49.33%) - LeetSync

---
+++ 3430-count-days-without-meetings/count-days-without-meetings.cpp
@@ -0,0 +1,26 @@
+    bool cmp(vector<int> &a, vector<int> &b) {
+        return a[0] < b[0];
+    }
+
+class Solution {
+public:
+    int countDays(int days, vector<vector<int>>& m) {
+        int n = m.size();
+        sort(m.begin(), m.end(), cmp);
+        int count = 0;
+        count += abs(m[0][0] - 1);
+
+        for(int i=1; i<n; i++) {
+            if(m[i][0] <= m[i-1][1]) { // overlapping
+                if(m[i][1] < m[i-1][1]) {
+                    m[i][1] = m[i-1][1]; // merge if intervals are overlapping
+                }
+            } else { // space exists between intervals
+                int dy = m[i][0] - m[i-1][1];
+                count += dy - 1;
+            }
+        }
+        count += abs(m[n-1][1] - days);
+        return count;
+    }
+};
