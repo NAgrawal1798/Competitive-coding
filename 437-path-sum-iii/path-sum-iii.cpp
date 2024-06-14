af2df3ff66021590bcc8e5bddf1baf64c475c1d1
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-06-14 07:46:50 +0530

    Time: 19 ms (48.32%) | Memory: 17.5 MB (59.18%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -1,20 +1,46 @@
+#include <iostream>
+#include <unordered_map>
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    int p(TreeNode* root, long long s) {
-        if(!root) {
-            return 0;
+    void helper(TreeNode* root, long long targetSum, int& ans) {
+        if (root == NULL) {
+            return;
         }
-        int res = 0;
-        s -= root->val;
-        if(s == 0) {
-            res += 1;
+        
+        targetSum -= root->val;
+        if (targetSum == 0) {
+            ans++;
         }
-        return res += p(root->right, s) + p(root->left, s);
+        
+        helper(root->left, targetSum, ans);
+        helper(root->right, targetSum, ans);
     }
-    int pathSum(TreeNode* root, int s) {
-       if(!root) {
-        return 0;
-       }
-       return pathSum(root->left, s) + p(root, s) + pathSum(root->right, s);
+
+    void pathSumHelper(TreeNode* root, long long targetSum, int& ans) {
+        if (root == NULL) {
+            return;
+        }
+        
+        helper(root, targetSum, ans);
+        pathSumHelper(root->left, targetSum, ans);
+        pathSumHelper(root->right, targetSum, ans);
+    }
+
+    int pathSum(TreeNode* root, int targetSum) {
+        int ans = 0;
+        pathSumHelper(root, targetSum, ans);
+        return ans;
     }
 };
