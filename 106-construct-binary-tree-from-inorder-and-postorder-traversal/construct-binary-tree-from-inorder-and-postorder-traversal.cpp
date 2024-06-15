cf0700ee7d5f3833a2173bff40df6eff61d0bfbd
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-06-15 17:01:52 +0530

    Time: 14 ms (46.88%) | Memory: 26.2 MB (26.95%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -11,39 +11,25 @@
  */
 class Solution {
 public:
-    // Inorder -> Left, Root, Right
-    // Postorder -> Left, Right, Root
-    // Preorder -> Root, Left, Right 
-
-    TreeNode* helper(vector<int>& inorder, vector<int>& postorder, int inorder_start, int inorder_end,
-    int postorder_start, int postorder_end, unordered_map<int, int>& mp) {
-    if (inorder_start > inorder_end || postorder_start > postorder_end) {
-        return nullptr;
+    TreeNode* build(vector<int>& inorder, vector<int>& postorder, unordered_map<int,int>&mp,
+    int inStart, int inEnd, int postStart, int postEnd) {
+        if (inStart > inEnd) {
+            return nullptr;
+        }
+        TreeNode* root = new TreeNode(postorder[postEnd]);
+        int index = mp[root->val];
+        int leftLen = index - inStart;
+        root->left = build(inorder, postorder, mp,  inStart, index - 1, postStart, postStart + leftLen - 1);
+        root->right = build(inorder, postorder,mp,  index + 1, inEnd, postStart + leftLen , postEnd - 1);
+        return root;
     }
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        unordered_map<int,int>mp;
 
-    int node_val = postorder[postorder_end];
-    TreeNode* node = new TreeNode(node_val);
+        for(int i=0; i<inorder.size(); i++) {
+            mp[inorder[i]] = i;
+        }
 
-    int node_present_inorder = mp[node_val];
-    int left_subtree_size = node_present_inorder - inorder_start;
-
-    node->left = helper(inorder, postorder, inorder_start, node_present_inorder - 1,
-                        postorder_start, postorder_start + left_subtree_size - 1, mp);
-    node->right = helper(inorder, postorder, node_present_inorder + 1, inorder_end,
-                         postorder_start + left_subtree_size, postorder_end - 1, mp);
-
-    return node;
-}
-
-TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-    unordered_map<int, int> mp;
-    int inorder_size = inorder.size();
-    int postorder_size = postorder.size();
-
-    for (int i = 0; i < inorder_size; i++) {
-        mp[inorder[i]] = i;
+        return build(inorder, postorder, mp, 0, inorder.size() - 1, 0, postorder.size() - 1);
     }
-
-    return helper(inorder, postorder, 0, inorder_size - 1, 0, postorder_size - 1, mp);
-}
 };
