5f65ef82a705db4987667a87989ce1c791ffa637
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-06-15 16:26:14 +0530

    Time: 19 ms (70.80%) | Memory: 60.7 MB (21.28%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -1,55 +1,25 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-TreeNode* first;
-    TreeNode* sec;
-    void inorder(TreeNode* root, TreeNode* &prev) {
-        if (root == NULL) {
-            return;
+    vector<int> v;
+    int i=0;
+    void inorder(TreeNode* root){
+        if(!root) return;
+        inorder(root->left);
+        v.push_back(root->val);
+        inorder(root->right);
+    }
+    void check(TreeNode* root){
+        if(!root) return;
+        check(root->left);
+        if(v[i]!=root->val){
+            root->val = v[i];
         }
-        inorder(root->left, prev);
-        if (prev && prev->val > root->val) {
-            if (!first) {
-                first = prev;
-            } 
-                sec = root;
-            // }
-        }
-        prev = root;
-        inorder(root->right, prev);
+        i++;
+        check(root->right);
     }
     void recoverTree(TreeNode* root) {
-        TreeNode* prev = nullptr;
-        inorder(root, prev);
-        int temp = first->val;
-        first->val = sec->val;
-        sec->val = temp;
+        inorder(root);
+        sort(v.begin(),v.end());
+        check(root);
     }
-    // TreeNode*prev=nullptr,*first=nullptr,*sec=nullptr;
-    // void inorder(TreeNode*cur){
-    //     if(!cur){return;}
-    //     inorder(cur->left);
-    //     if(prev!=NULL && cur->val<prev->val){ // check if current value less than previous value that should not be possible in inorder traversal.
-    //         if(!first){first=prev;}
-    //         sec=cur;
-    //     }
-    //     prev=cur;
-    //     inorder(cur->right);
-    // }
-    // void recoverTree(TreeNode* root) {
-    //     inorder(root);
-    //     int temp=first->val;
-    //     first->val=sec->val;
-    //     sec->val=temp;
-    // }
 };
