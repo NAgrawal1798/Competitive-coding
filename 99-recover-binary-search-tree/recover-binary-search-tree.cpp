be774594ec3635128fcade55a342218bfa283d6f
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-06-15 16:33:59 +0530

    Time: 24 ms (41.45%) | Memory: 60.2 MB (91.38%) - LeetSync

--- 99-recover-binary-search-tree/recover-binary-search-tree.cpp
+++ 99-recover-binary-search-tree/recover-binary-search-tree.cpp
@@ -1,25 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+    TreeNode* first, *last, *prev;
 public:
-    vector<int> v;
-    int i=0;
-    void inorder(TreeNode* root){
-        if(!root) return;
+    void inorder(TreeNode* root) {
+        if(root == NULL) {
+            return;
+        }
         inorder(root->left);
-        v.push_back(root->val);
+        if (prev != NULL && root->val < prev->val) {
+            if(first == NULL) {
+                first = prev;
+                last = root;
+            } else {
+                last = root;
+            }
+        }
+        prev = root;
         inorder(root->right);
     }
-    void check(TreeNode* root){
-        if(!root) return;
-        check(root->left);
-        if(v[i]!=root->val){
-            root->val = v[i];
-        }
-        i++;
-        check(root->right);
-    }
     void recoverTree(TreeNode* root) {
+        first = last = prev = NULL;
         inorder(root);
-        sort(v.begin(),v.end());
-        check(root);
+        swap(first->val, last->val);
     }
 };
