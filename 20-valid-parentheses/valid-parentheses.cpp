9b49a3fae9d8bac95359cf49ebdc08f194035c17
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-06-25 07:51:54 +0530

    Time: 3 ms (41.63%) | Memory: 7.6 MB (33.42%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -3,35 +3,39 @@ public:
     bool isValid(string s) {
         stack<char>st;
         int n = s.size();
+
         for(int i=0; i<n; i++) {
-            if(s[i] == ')' || s[i] == '}' || s[i] == ']') {
-                if(st.empty()) {
-                    return false;
-                }
-                if(s[i] == ')') {
-                    char p = st.top();
-                    cout<<p<<endl;
-                    if(p != '(') {
-                        return false;
-                    }
-                }
-                if(s[i] == '}') {
-                    char p = st.top();
-                    if(p != '{') {
-                        return false;
-                    }
-                }
-                if(s[i] == ']') {
-                    char p = st.top();
-                    if(p != '[') {
-                        return false;
-                    }
-                }
-                st.pop();
-            } else {
+            if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
                 st.push(s[i]);
+            } else if (st.empty()) {
+                return false;
+            } else{
+                if (s[i] == ')') {
+                    if (st.top() == '(') {
+                        st.pop();
+                    } else {
+                        return false;
+                    }
+                } else if (s[i] == ']') {
+                    if (st.top() == '[') {
+                        st.pop();
+                    } else {
+                        return false;
+                    }
+                } else {
+                    if (st.top() == '{') {
+                        st.pop();
+                    } else {
+                        return false;
+                    }
+                }
             }
         }
-        return st.empty();
+
+        // 
+        if (st.empty()) {
+            return true;
+        }
+        return false;
     }
 };
