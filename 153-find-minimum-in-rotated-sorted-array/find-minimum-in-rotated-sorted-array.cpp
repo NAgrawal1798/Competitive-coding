e870805b431013e58e137c7c85514602324d787b
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-06-26 22:45:43 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (59.96%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -1,40 +1,22 @@
 class Solution {
 public:
-    int findPivot(vector<int>& arr, int low, int high)
-{
-    // Base cases
-    if (high < low)
-        return -1;
-    if (high == low)
-        return low;
- 
-    // low + (high - low)/2;
-    int mid = (low + high) / 2;
-    if (mid < high && arr[mid] > arr[mid + 1])
-        return mid;
- 
-    if (mid > low && arr[mid] < arr[mid - 1])
-        return (mid - 1);
- 
-    if (arr[low] >= arr[mid])
-        return findPivot(arr, low, mid - 1);
- 
-    return findPivot(arr, mid + 1, high);
-}
     int findMin(vector<int>& nums) {
         int n = nums.size();
-        int pivot = findPivot(nums, 0, n - 1);
- 
-        // If we didn't find a pivot,
-        // then array is not rotated at all
-        if (pivot == n-1) {
+        int low=0, high=n-1;
+        if (n==1) {
             return nums[0];
         }
-        if(n == 1) {
-            return nums[0];
+        
+        while(low<high){
+            if(nums[low] <= nums[high]) return nums[low];
+            int mid = low + (high-low)/2;
+            if(nums[low] > nums[mid]){
+                high=mid;
+            } else if(nums[mid] > nums[high]) {
+                low=mid+1;
+            } 
         }
-
-        return nums[pivot+1];
-
+        if(nums[low] <= nums[high]) return nums[low];
+        return -1;
     }
 };
