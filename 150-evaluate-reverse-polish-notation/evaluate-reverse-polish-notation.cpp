27bc6a392fe5afbdafc1345c6e3bccef79093d91
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-06-26 08:28:36 +0530

    Time: 7 ms (67.65%) | Memory: 15.5 MB (55.75%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -1,48 +1,51 @@
 class Solution {
 public:
-    // use stack
-    int performOperation(string x, string y, string sign) {
-        int first = stoi(x);
-        int second = stoi(y);
-        if(sign == "+") {
-            return first + second;
+    int eval(int a, int b, char sign) {
+        switch (sign) {
+            case '+':
+                return a + b;
+            case '-':
+                return a - b;
+            case '*':
+                return a * b;
+            case '/':
+                return a/b;
+            default:
+                return 0;
         }
-        if (sign == "*") {
-            return first * second;
-        }
-        if (sign == "/") {
-            return first/second;
-        }
-        if (sign == "-") {
-            return first-second;
-        }
-        return 0;
-    }
-
-    bool isSymbol(string token) {
-        if (token == "+" || token == "-" || token == "/" || token == "*") {
-            return true;
-        }
-        return false;
     }
     int evalRPN(vector<string>& tokens) {
-        stack<string>s;
+        stack<int>s;
         int n = tokens.size();
         for(int i=0; i<n; i++) {
-            if(isSymbol(tokens[i])) {
-                string y = s.top();  // Pop the second operand first
+            if (tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/") {
+                int b = s.top();
                 s.pop();
-                string x = s.top();
+                int a = s.top();
                 s.pop();
-                int res = performOperation(x, y, tokens[i]);
-                string result = to_string(res);
-                s.push(result);
+                if (tokens[i] == "+") {
+                    int res = eval(a, b, '+');
+                    s.push(res);
+                }
+                if (tokens[i] == "-") {
+                    int res = eval(a, b, '-');
+                    s.push(res);
+                }
+                if (tokens[i] == "*") {
+                    int res = eval(a, b, '*');
+                    s.push(res);
+                }
+                if (tokens[i] == "/") {
+                    int res = eval(a, b, '/');
+                    s.push(res);
+                }
             } else {
-                s.push(tokens[i]);
+                s.push(stoi(tokens[i]));
             }
         }
-        string ans = s.top();
-        int result = stoi(ans);
+
+        int result = s.top();
         return result;
+
     }
 };
