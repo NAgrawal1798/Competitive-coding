db355119b297a82efb9d3f27a881df9dac247433
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-06-29 18:00:28 +0530

    Time: 35 ms (20.30%) | Memory: 10.6 MB (52.80%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        // expand and shrink concept;
-        string ans;
+        map<char, int>mp;
+        int n = s.size();
+        int m = t.size();
+
         int left = 0;
         int right = 0;
-        int minStart = 0;
-        int minLen = INT_MAX;
-        int m = s.size();
-        int n = t.size();
-        map<char, int>mp;
-        for(int k=0; k<n; k++) {
-            mp[t[k]]++;
+        for (int i=0; i<m; i++) {
+            mp[t[i]]++;
         }
-
-        int remaining = t.length();
-        while(right != m) {
-            if(mp.find(s[right]) != mp.end()) {
+        int remaining = m;
+        int minStart = 0;
+        int minLen = INT_MAX;   
+        while(right < n) {
+            if (mp.find(s[right]) != mp.end()) {
                 mp[s[right]]--;
-                if(mp[s[right]] >= 0) { // Checking if the character at right pointer is needed
+                if (mp[s[right]] >= 0) {
                     remaining--;
                 }
             }
             right++;
-
             while(remaining == 0) {
-                if(right - left < minLen) {
+                if (right - left < minLen) {
                     minLen = right - left;
                     minStart = left;
                 }
-                if(mp.find(s[left]) != mp.end()) {
+
+                if (mp.find(s[left]) != mp.end()) {
                     mp[s[left]]++;
-                    if(mp[s[left]] > 0) { // Checking if the character at left pointer is no longer present in window
+                    if (mp[s[left]] > 0) {
                         remaining++;
                     }
                 }
                 left++;
             }
-}
+        }
 
         return minLen < INT_MAX ? s.substr(minStart, minLen) : "";
     }
