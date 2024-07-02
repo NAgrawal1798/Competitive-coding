b6fde01c12a8bf4b56b42510c99dc42be6fbf26b
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-07-02 22:30:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (91.55%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        vector<int>changeMethodCount(amount + 1 ,0);
+
+        changeMethodCount[0] = 1;
+        for (int coin: coins) {
+            for (int smallAmount = coin; smallAmount <= amount; smallAmount++) {
+                changeMethodCount[smallAmount] += changeMethodCount[ smallAmount - coin ];
+            }
+        }
+
+        return changeMethodCount[amount];
+    }
+};
