b543ae88d90daaaaf33f00f1f1f2f16eeeec9a5b
Nikhil Agrawal <43701608+NAgrawal1798@users.noreply.github.com>
2024-07-03 08:52:49 +0530

    Time: 87 ms (67.66%) | Memory: 23.5 MB (89.83%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
-    //  Apply dijstra algorithm
-    //  First pop element which is at the right bottom index is the answer
     int minimumEffortPath(vector<vector<int>>& heights) {
         int n = heights.size();
         int m = heights[0].size();
 
-        priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>,
-        greater<pair<int,pair<int,int>>>>pq;
+        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>,
+        greater<pair<int, pair<int, int>>>>pq;
 
         vector<vector<int>>dist(n, vector<int>(m, 1e9));
-        dist[0][0]=0;
+        dist[0][0] = 0;
         pq.push({0, {0,0}});
 
         vector<int>dr = {-1, 0, 1, 0};
@@ -19,6 +17,7 @@ public:
         while(!pq.empty()) {
             auto it = pq.top();
             pq.pop();
+
             int diff = it.first;
             int row = it.second.first;
             int col = it.second.second;
@@ -27,11 +26,11 @@ public:
                 return diff;
             }
 
-            for (int i=0; i<4; i++) {
+            for(int i=0; i<4; i++) {
                 int newr = row + dr[i];
                 int newc = col + dc[i];
 
-                if (newr >= 0 && newr < n && newc >=0 && newc <m) {
+                if (newr >=0 && newr < n && newc >=0 && newc < m) {
                     int newEffort = max(abs(heights[row][col] - heights[newr][newc]), diff);
 
                     if (newEffort < dist[newr][newc]) {
@@ -41,6 +40,7 @@ public:
                 }
             }
         }
+
         return 0;
     }
 };
